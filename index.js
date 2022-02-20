cee0944b3a80aac3d06649030e7967e074dcaffe
Jacob Hooser <jacob.hooser@yahoo.com>
2022-02-20 11:37:03 -0600

    commit tree 88267903e765db880d3838bbffc5738992cb5521

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  }
+  else if (someValue > 400 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someValue){
   // Write your code here!
+  if(someValue == 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else
+    return 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someValue){
   // Write your code here!
+  if(someValue == 'generous'){
+    return 'Thank you so much.';
+  }
+  else if (someValue == 'not as generous'){
+    return 'Thank you.';
+  }
+  else 
+    return 'Bye.';
 }
